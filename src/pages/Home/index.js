b9ff9d4944f821ae52d97c38a4cc8d78da0fdb14
Taima <gustavotaima18@gmail.com>
2023-02-08 11:43:42 -0300

    Inicio da tela home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { View, StyleSheet } from "react-native";
+
+export default function Home() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
