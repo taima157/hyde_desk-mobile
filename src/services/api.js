4f14826dbf221ca729a250fbbfdf5844189d2877
Leonardo Lopes <leolopesribeiro05@gmail.com>
2023-03-08 16:57:30 -0300

    BaseUrl arrumada

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://hdteste.azurewebsites.net/",
-  baseURL: "http://192.168.15.10:4001/",
+  baseURL: "https://hdteste.azurewebsites.net/",
+  // baseURL: "http://192.168.15.10:4001/",
 });
